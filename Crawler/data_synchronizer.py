08906e9c3b0abdaa278f6c9b091efb178416b31b
jehyunJung <toojey7@gmail.com>
2020-03-13 17:31:24 +0900

    feat: add twitter data synchronizer

---
+++ Crawler/data_synchronizer.py
@@ -0,0 +1,8 @@
+from .twitter_crawler import Twitter_Crawler
+
+def synchronizer(**kwagrs):
+    twitter_api=Twitter_Crawler()
+    twitter_api.crawl_by_keyword(kwagrs['keyword'],kwagrs['pages'])
+
+if __name__ =="__main__":
+    synchronizer(keyword='코로나',pages=100)
---
+++ Crawler/twitter_crawler.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import json
+import tweepy
+
+class Twitter_Crawler():
+    #Twitter API를 사용하기 위해서, 키를 입력해서 twitter.api.API 객체를 생성한다.
+    def __init__(self):
+        with open('../server_config.json', encoding='utf-8') as fp:
+            configs=json.load(fp)
+            self.consumer_key=configs['CONSUMER_KEY']
+            self.consumer_secret=configs['CONSUMER_SECRET']
+            self.access_token_key=configs['ACCESS_TOKEN_KEY']
+            self.access_token_secret=configs['ACCESS_TOKEN_SECRET']
+
+        auth=tweepy.OAuthHandler(self.consumer_key,self.consumer_secret)
+        auth.set_access_token(self.access_token_key,self.access_token_secret)
+        self.api=tweepy.API(auth)
+
+    def crawl_by_keyword(self,keyword,pages):
+        columns = ['created', 'text']
+        df = pd.DataFrame(columns=columns)
+
+        for page in range(pages):
+            tweets = self.api.search(keyword,lang='ko')
+            print(page,' : ',len(tweets))
+            for tweet in tweets:
+                tweet_text = tweet.text
+                created = tweet.created_at
+                row = [created, tweet_text]
+                series = pd.Series(row, index=df.columns)
+                df = df.append(series, ignore_index=True)
+
+        df.to_csv('../data/twitter_data.csv',index=False,encoding='utf-8')
+
