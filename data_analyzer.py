db3411e23e3c13fe33c36e2cfbca98c1a82e47f9
jehyunJung <toojey7@gmail.com>
2020-03-13 20:03:09 +0900

    feat: completed main execution model

--- data_analyzer.py
+++ data_analyzer.py
@@ -1,13 +1,16 @@
 import pandas as pd
 from Preprocessor.data_preprocessor import Preprocessor
+from relation_analyzer import Relation_Analyzer
 
 def main():
+    #twitter 데이터를 읽는다
     data=pd.read_csv('./data/twitter_data.csv')
 
     #데이터 내의 'text' column 전처리를 진행한다.
-    data_preprocessor=Preprocessor()
-    data_preprocessor.data_Preprocessing(data,'text')
+    Preprocessor.data_Preprocessing(data,'text')
 
+    #전처리된 데이터를 이용해서 연관 분석을 진행한다.
+    Relation_Analyzer.analyze(data['target'])
 
 if __name__ == "__main__":
     main()
