b50d42f28043d3dd3da00a91641620cdb6342b14
jehyunJung <toojey7@gmail.com>
2020-03-13 19:04:14 +0900

    style: simplified data_analyzer.py

--- data_analyzer.py
+++ data_analyzer.py
@@ -1,34 +1,12 @@
 import pandas as pd
-import re
-import konlpy.tag import Okt
-
-#정규식을 이용해서 한글만을 남기도록 한다.
-def korean_Extract(text):
-    korean=re.compile('[^ ㄱ-ㅣ가-힣]+')
-    result=korean.sub('',text)
-    return result
-
-#konlpy 패키지를 활용해서 명사(2글자 이상)만 남기도록 한다.
-def noun_Extract(text):
-    nouns_extractor=Okt()
-    nouns=nouns_extractor.nouns(text)
-    nouns=[noun for noun in nouns if len(noun) >1]
-    return nouns
-#Stopwords(불용어)를 제거하도록 한다.
-def delete_Stopwords(text_data):
-    stopwords=[]
-    with open('./stopwords.txt', encoding='utf-8') as fp:
-        stopwords=fp.readlines()
-    stopwords=[stopword.strip() for stopword in stopwords]
-    nouns=[text for text in text_data if text not in stopwords]
-    return nouns
-
-def data_Preprocessing(data):
-
-
+from Preprocessor.data_preprocessor import Preprocessor
 
 def main():
     data=pd.read_csv('./data/twitter_data.csv')
+
+    #데이터 내의 'text' column 전처리를 진행한다.
+    data_preprocessor=Preprocessor()
+    data_preprocessor.data_Preprocessing(data,'text')
 
 
 if __name__ == "__main__":
