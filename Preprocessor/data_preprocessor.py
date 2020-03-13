529289b7b73da3ecdefe16f4952da13322575cd0
jehyunJung <toojey7@gmail.com>
2020-03-13 19:22:11 +0900

    style: changed Data Preprocessor model to static

--- Preprocessor/data_preprocessor.py
+++ Preprocessor/data_preprocessor.py
@@ -4,19 +4,22 @@ from konlpy.tag import Okt
 
 class Preprocessor:
     #정규식을 이용해서 한글만을 남기도록 한다.
-    def korean_Extract(self,text):
+    @staticmethod
+    def korean_Extract(text):
         korean=re.compile('[^ ㄱ-ㅣ가-힣]+')
         result=korean.sub('',text)
         return result
 
     #konlpy 패키지를 활용해서 명사(2글자 이상)만 남기도록 한다.
-    def noun_Extract(self,text):
+    @staticmethod
+    def noun_Extract(text):
         nouns_extractor=Okt()
         nouns=nouns_extractor.nouns(text)
         nouns=[noun for noun in nouns if len(noun) >1]
         return nouns
     #Stopwords(불용어)를 제거하도록 한다.
-    def delete_Stopwords(self,text_data):
+    @staticmethod
+    def delete_Stopwords(text_data):
         stopwords=[]
         with open('./stopwords.txt', encoding='utf-8') as fp:
             stopwords=fp.readlines()
@@ -24,8 +27,9 @@ class Preprocessor:
         nouns=[text for text in text_data if text not in stopwords]
         return nouns
 
-    def data_Preprocessing(self,data,column):
-        data['preprocessed_'+column]=data[column].apply(lambda text:self.korean_Extract(text)).apply(lambda text:self.noun_Extract()).apply(lambda text:self.delete_Stopwords())
+    @staticmethod
+    def data_Preprocessing(data,column):
+        data['preprocessed_'+column]=data[column].apply(lambda text:Preprocessor.korean_Extract(text)).apply(lambda text:Preprocessor.noun_Extract()).apply(lambda text:Preprocessor.delete_Stopwords())
 
 
 
