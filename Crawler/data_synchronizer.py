f6b66a79f75fa308314af08a8f99ea3547a1b0ff
jehyunJung <toojey7@gmail.com>
2020-03-21 00:04:06 +0900

    update: change crawling access methods by inserting arguments

--- Crawler/data_synchronizer.py
+++ Crawler/data_synchronizer.py
@@ -1,8 +1,27 @@
 from twitter_crawler import Twitter_Crawler
+import sys
 
-def synchronizer(**kwagrs):
+def synchronizer(keyword,pages=50):
     twitter_api=Twitter_Crawler()
-    twitter_api.crawl_by_keyword(kwagrs['keyword'],kwagrs['pages'])
+    twitter_api.crawl_by_keyword(keyword,pages)
 
 if __name__ =="__main__":
-    synchronizer(keyword='손흥민',pages=10)
+    if len(sys.argv) < 2:
+        print('Wrong Execution')
+        print('Try python ./data_synchronizer -k <keyword> -p <pages>[default page option is 50]')
+    elif sys.argv[1]=='-k':
+        if len(sys.argv)>2 :
+            keyword=sys.argv[2]
+            if sys.argv[3]=='-p':
+                if len(sys.argv)>4:
+                    pages=int(sys.argv[4])
+                    synchronizer(keyword,pages)
+                else:
+                    print('page is not inserted')
+            else:
+                synchronizer(keyword)
+        else:
+            print('Keyword not inserted')
+
+    else:
+        print('Wrong option')
